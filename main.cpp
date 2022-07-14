a28b365e11ae5402dd1307632c7b73fe0fd656cd
Julian Siefert <juliansiefert@Macpro.local>
2022-07-14 21:18:35 +0200

    Added Main and Adminmenu in main.cpp

--- main.cpp
+++ main.cpp
@@ -3,22 +3,72 @@
 #include <stdlib.h>
 #include <cstdlib>
 using namespace std;
-void mainmenu();
+int mainmenu();
+void adminmenu();
+int login();
 int main(){
-	mainmenu();
+	cout << mainmenu();
 }
-void mainmenu(){
-	//system("clear");
-	cout << "==========Hauptmenü=========="<< endl
-		 << "\t1)Erster Menuepunkt" << endl
-		 << "\t2)Zweiter Menuepunkt" << endl
-		 << "\t3)Dritter Menuepunkt" << endl
-		 << "\t4)Vierter Menuepunkt" << endl
-		 << "\t5)Fuenfter Menuepunkt" << endl
-		 << "\t6)Sechster Menuepunkt" << endl
-		 << "=============================";
-	char delscreen;
-	cin >> delscreen;
-	cout <<"\033c";
-	return;
+
+int  mainmenu(){
+	int eingabe;
+	bool check = true;
+	do{
+	cout << "=======Banksystem V0.01======" << endl
+		 << "==========Hauptmenü=========="<< endl
+		 << "1)Erster Menuepunkt" << endl
+		 << "2)Zweiter Menuepunkt" << endl
+		 << "3)Dritter Menuepunkt" << endl
+		 << "4)Vierter Menuepunkt" << endl
+		 << "5)Fuenfter Menuepunkt" << endl
+		 << "6)Sechster Menuepunkt" << endl
+		 << "0)Adminmenue" << endl
+		 << "=============================" << endl
+		 << "Bitte Treffen Sie eine Auswahl ";
+
+	cin >> eingabe;
+	if(eingabe >= 1 && eingabe <=6){
+		check = false;
+	}
+	if(eingabe == 0){
+		adminmenu();
+	}
+	}
+	while(check);
+
+	return eingabe;
+}
+void adminmenu(){
+	if(!login()){
+		cout << "Willkommen im Adminmenü" << endl;
+
+	}
+}
+int login(){
+	string dbnutzer = "Admin";
+	string dbpw = "passwort";
+	string nutzer;
+	string pw;
+	int status = 1;
+	int i = 0;
+	 cout << "Bitte melden Sie sich mit Ihren Nutzerdaten an:" << endl;
+	 while(i <3){
+	 cout << "Benutzername:";
+	 cin >> nutzer;
+	 cout << "Passwort:";
+	 cin >> pw;
+
+	 if(dbnutzer == nutzer && dbpw == pw){
+		 cout << "Sie haben sich erfolgreich angemeldet!" << endl;
+				 status = 0;
+				 return status;
+	 }
+	 i++;
+	 cout << "Sie haben das Passwort bereits " << i << "mal falsch eingegeben"<< endl;
+	 cout << "Sie haben " << 3-i << " Versuche uebrig" << endl;
+	 }
+	 cout << "Sie haben zu oft falsche Daten eingegeben!" << endl;
+	 exit(1);
+	 return status;
+
 }

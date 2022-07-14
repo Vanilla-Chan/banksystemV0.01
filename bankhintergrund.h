fc2f496945905e71a212f3ea942c14805a284281
Julian Siefert <juliansiefert@Macpro.local>
2022-07-14 15:31:15 +0200

    Added essential variables and functions to header

--- bankhintergrund.h
+++ bankhintergrund.h
@@ -7,31 +7,47 @@
 class Benutzer{
 private:
   int benutzerID;
-  std::string name;
+  std::string vorname;
   std::string nachname;
   std::string telefonnummer;
   std::string adresse;
   std::string geburtsdatum;
 public:
-  Benutzer();
+  Benutzer(std::string iName, std::string iNachname,std::string iTelefonnummer,std::string iAdresse, std::String iGeburtsdatum);
+  std::string getVorname();
+  std::string getNachname();
+  std::string getTelefonnummer();
+  std::string getAdresse();
+  std::string getGeburtsdatum();
 };
 class Konto{
 private:
   int kontonummer;
   int benutzerID;
   float kontostand;
+public:
+  Konto(int iKontonummer,int iBenutzerID,float iKontostand);
+  int getKontonummer();
+  int getBenutzerID();
+  float getkontostand();
+  void setKontostand(float iKontostand);
 };
-class Ueberweisung{
+class Transaktion{
 private:
   int sendkontoID;
   int empfkontoID;
   float betrag;
   std::string verwendungszweck;
+public:
+  void ueberweisung(Konto& sender, Konto& empfaenger, float betrag);
+  void auszahlen(Konto&, float betrag);
+  void einzahlen(Konto&,float betrag);
 };
 class Banksystem{
 private:
-  vector<Benutzer> Kunden;
-  vector<Konto> Konten;
-  vector<Ueberweisung> Ueberweisungen;
+  map<int,Benutzer> Kunden;
+  map<int,Konto> Konten;
+  map<int,Transaktion> Transaktionen;
+  map<int,Vorgang> Vorgaenge;
 };
 #endif

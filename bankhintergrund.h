d18871a74d696765791214cb11b587add7d1538b
Julian Siefert <juliansiefert@Macpro.local>
2022-07-14 15:00:22 +0200

    Added Classes Konto and Besitzer to header file

--- bankhintergrund.h
+++ bankhintergrund.h
@@ -1,8 +1,15 @@
 #pragma once
 #ifndef BANKHINTERGRUND_H_
-#define BANKHINTERGRUND_H:_
+#define BANKHINTERGRUND_H_
+class Konto{
+
+};
+class Besitzer{
+
+};
+
 class Banksystem{
-  
+
 };
 
 #endif

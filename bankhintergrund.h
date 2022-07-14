003b17eb9316e39347f16d51dc177c43875633b6
Julian Siefert <juliansiefert@Macpro.local>
2022-07-14 15:08:32 +0200

    Renamed Beitzer into benutzer and implemeted classes not done yet

--- bankhintergrund.h
+++ bankhintergrund.h
@@ -2,14 +2,30 @@
 #pragma once
 #ifndef BANKHINTERGRUND_H_
 #define BANKHINTERGRUND_H_
-class Besitzer{
+#include <string>
+#include <vector>
+class Benutzer{
+private:
+  int benutzerID;
+  std::string name;
+  std::string nachname;
+  std::string telefonnummer;
+  std::string adresse;
+  std::string geburtsdatum;
 
 };
 
-class Banksystem{
-
-};
 class Konto{
+  int kontonummer;
+  int benutzerID;
+  float kontostand;
+
+};
+class Ueberweisugn{};
+class Banksystem{
+private:
+  vector<Benutzer> USER;
+  vector<Konto> KONTEN;
 
 };
 #endif
